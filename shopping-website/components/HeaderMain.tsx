1db26b281a504edb24a54e8fb07b014f15e0ba79
Abhishek Peiris <abhisheklpeiris@gmail.com>
2024-06-25 10:47:52 +0530

    Add logo

--- shopping-website/components/HeaderMain.tsx
+++ shopping-website/components/HeaderMain.tsx
@@ -3,12 +3,20 @@ import { BiUser } from 'react-icons/bi'
 import { BsSearch } from 'react-icons/bs'
 import { FiHeart } from 'react-icons/fi'
 import { HiOutlineShoppingBag } from 'react-icons/hi'
+import Image from 'next/image'
+import Logo from '../public/shopping-logo.png'
 
 function HeaderMain() {
   return (
     <div className='border-b border-gray-200 py-6'>
         <div className='container sm:flex justify-between items-center'>
-            <div className='font-bold text-4xl text-center pb-4 sm:pb-0 text-blackish'>Logo</div>
+            <div className='font-bold text-4xl text-center pb-4 sm:pb-0 text-blackish'>
+                <Image 
+                    src={Logo}
+                    alt="banner"
+                    width={200}
+                />
+            </div>
         
             <div className='w-full sm:w-[300px] md:w-[70%] relative'>
                 <input className='border-gray-200 border p-2 px-4 rounded-lg w-full' type="text" placeholder='Enter any product name...' />
