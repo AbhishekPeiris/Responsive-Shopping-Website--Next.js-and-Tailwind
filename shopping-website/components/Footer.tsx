3bd433f72026f43565543f520b122fa06200b4f4
Abhishek Peiris <abhisheklpeiris@gmail.com>
2024-06-25 21:51:40 +0530

    footer

---
+++ shopping-website/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='bg-blackish text-gray-500 text-center py-4 pb-16 md:pb-4'>
+        Copyright &copy; Online shopping <br/> All Rights Reserved 2024
+    </div>
+  )
+}
+
+export default Footer
