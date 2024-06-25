0119be389eeb8bbcc8810f7a296f3402884eda5d
Abhishek Peiris <abhisheklpeiris@gmail.com>
2024-06-25 20:14:16 +0530

    about

--- shopping-website/components/ProductCard.tsx
+++ shopping-website/components/ProductCard.tsx
@@ -73,7 +73,7 @@ const ProductCard: React.FC<propsType> = (
   }
 
   return (
-    <div className='px-4 border-gray-200 rounded-xl max-w-[400px]'>
+    <div className='px-4 border border-gray-200 rounded-xl max-w-[400px]'>
         <div>
             <Image className='w-full h-auto' src={img} width={200} height={300} alt={title}/>
         </div>
